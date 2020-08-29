e6845e324702d07ecd6b65178f05b21ab6c85d3e
o4x <52994716+o4x@users.noreply.github.com>
2020-08-29 21:44:10 +0430

    Fix about

---
+++ app/src/main/java/com/o4x/musical/Constants.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019 Hemanth Savarala.
+ *
+ * Licensed under the GNU General Public License v3
+ *
+ * This is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by
+ *  the Free Software Foundation either version 3 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
+
+package com.o4x.musical
+
+import android.provider.BaseColumns
+import android.provider.MediaStore
+
+object Constants {
+    const val PRO_VERSION_PRODUCT_ID = "pro_version"
+    const val RATE_ON_GOOGLE_PLAY =
+        "https://play.google.com/store/apps/details?id=code.name.monkey.retromusic"
+    const val TRANSLATE = "https://github.com/h4h13/RetroMusicPlayer"
+    const val GITHUB_PROJECT = "https://github.com/h4h13/RetroMusicPlayer"
+    const val TELEGRAM_CHANGE_LOG = "https://t.me/retromusiclog"
+    const val USER_PROFILE = "profile.jpg"
+    const val USER_BANNER = "banner.jpg"
+    const val APP_INSTAGRAM_LINK = "https://www.instagram.com/retromusicapp/"
+    const val APP_TELEGRAM_LINK = "https://t.me/retromusicapp/"
+    const val APP_TWITTER_LINK = "https://twitter.com/retromusicapp"
+    const val FAQ_LINK = "https://github.com/h4h13/RetroMusicPlayer/blob/master/FAQ.md"
+    const val PINTEREST = "https://in.pinterest.com/retromusicapp/"
+    const val BASE_URL = "https://ws.audioscrobbler.com/2.0/"
+}
---
+++ app/src/main/java/com/o4x/musical/ui/fragments/about/AboutFragment.kt
@@ -0,0 +1,109 @@
+package com.o4x.musical.ui.fragments.about
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.ShareCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.o4x.musical.App
+import com.o4x.musical.Constants
+import com.o4x.musical.R
+import com.o4x.musical.util.NavigationUtil
+import kotlinx.android.synthetic.main.card_other.*
+import kotlinx.android.synthetic.main.card_retro_info.*
+import kotlinx.android.synthetic.main.card_social.*
+import org.eclipse.egit.github.core.Contributor
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+
+class AboutFragment : Fragment(R.layout.fragment_about), View.OnClickListener {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        version.setSummary(getAppVersion())
+        setUpView()
+    }
+
+    private val contributorsJson: String?
+        get() {
+            val json: String
+            try {
+                val inputStream = requireActivity().assets.open("contributors.json")
+                val size = inputStream.available()
+                val buffer = ByteArray(size)
+                inputStream.read(buffer)
+                inputStream.close()
+                json = String(buffer, StandardCharsets.UTF_8)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+                return null
+            }
+            return json
+        }
+
+
+    private fun openUrl(url: String) {
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(i)
+    }
+
+    private fun setUpView() {
+        appGithub.setOnClickListener(this)
+        faqLink.setOnClickListener(this)
+        telegramLink.setOnClickListener(this)
+        appRate.setOnClickListener(this)
+        appTranslation.setOnClickListener(this)
+        appShare.setOnClickListener(this)
+        donateLink.setOnClickListener(this)
+        instagramLink.setOnClickListener(this)
+        twitterLink.setOnClickListener(this)
+        changelog.setOnClickListener(this)
+        openSource.setOnClickListener(this)
+        pinterestLink.setOnClickListener(this)
+        bugReportLink.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.pinterestLink -> openUrl(Constants.PINTEREST)
+            R.id.faqLink -> openUrl(Constants.FAQ_LINK)
+            R.id.telegramLink -> openUrl(Constants.APP_TELEGRAM_LINK)
+            R.id.appGithub -> openUrl(Constants.GITHUB_PROJECT)
+            R.id.appTranslation -> openUrl(Constants.TRANSLATE)
+            R.id.appRate -> openUrl(Constants.RATE_ON_GOOGLE_PLAY)
+            R.id.appShare -> shareApp()
+            R.id.donateLink -> NavigationUtil.goToSupportDevelopment(requireActivity())
+            R.id.instagramLink -> openUrl(Constants.APP_INSTAGRAM_LINK)
+            R.id.twitterLink -> openUrl(Constants.APP_TWITTER_LINK)
+            R.id.changelog -> openUrl(Constants.TELEGRAM_CHANGE_LOG)
+            R.id.openSource -> NavigationUtil.goToOpenSource(requireActivity())
+            R.id.bugReportLink -> NavigationUtil.bugReport(requireActivity())
+        }
+    }
+
+    private fun getAppVersion(): String {
+        return try {
+            val isPro = if (App.isProVersion()) "Pro" else "Free"
+            val packageInfo =
+                requireActivity().packageManager.getPackageInfo(requireActivity().packageName, 0)
+            "${packageInfo.versionName} $isPro"
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            "0.0.0"
+        }
+    }
+
+    private fun shareApp() {
+        ShareCompat.IntentBuilder.from(requireActivity()).setType("text/plain")
+            .setChooserTitle(R.string.share_app)
+            .setText(String.format(getString(R.string.app_share), requireActivity().packageName))
+            .startChooser()
+    }
+}
