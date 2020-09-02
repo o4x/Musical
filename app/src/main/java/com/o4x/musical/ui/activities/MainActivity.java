de842ea794f9be657329a95fcd9272e290c0a92e
o4x <52994716+o4x@users.noreply.github.com>
2020-09-02 20:33:53 +0430

    Move about to fragment

--- app/src/main/java/com/o4x/musical/ui/activities/MainActivity.java
+++ app/src/main/java/com/o4x/musical/ui/activities/MainActivity.java
@@ -226,9 +226,6 @@ public class MainActivity extends AbsMusicPanelActivity {
                 case R.id.nav_settings:
                     new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, SettingsActivity.class)), 200);
                     break;
-                case R.id.nav_about:
-                    new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, AboutActivity.class)), 200);
-                    break;
             }
             return true;
         });
