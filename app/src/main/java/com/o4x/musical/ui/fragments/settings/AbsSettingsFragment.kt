8c3b7ea14bd1a1003bd9326b0ddaa00c3db51a28
o4x <52994716+o4x@users.noreply.github.com>
2020-09-03 10:24:35 +0430

    Show drawer coloe

--- app/src/main/java/com/o4x/musical/ui/fragments/settings/AbsSettingsFragment.kt
+++ app/src/main/java/com/o4x/musical/ui/fragments/settings/AbsSettingsFragment.kt
@@ -23,6 +23,8 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceManager
 import code.name.monkey.appthemehelper.common.prefs.supportv7.ATEPreferenceFragmentCompat
+import code.name.monkey.appthemehelper.util.ATHUtil
+import com.o4x.musical.R
 import com.o4x.musical.preferences.*
 import com.o4x.musical.util.NavigationUtil
 
@@ -60,11 +62,7 @@ abstract class AbsSettingsFragment : ATEPreferenceFragmentCompat() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setDivider(ColorDrawable(Color.TRANSPARENT))
-        //listView.setBackgroundColor(ATHUtil.resolveColor(requireContext(), R.attr.colorSurface))
-        listView.overScrollMode = View.OVER_SCROLL_NEVER
-        listView.setPadding(0, 0, 0, 0)
-        listView.setPaddingRelative(0, 0, 0, 0)
+        listView.setBackgroundColor(ATHUtil.resolveColor(requireContext(), R.attr.colorSurface))
         invalidateSettings()
     }
 
