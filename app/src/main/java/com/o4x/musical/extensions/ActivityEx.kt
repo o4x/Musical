1b1de8ca84a8b9dc50e03c31c524a64b261d100c
o4x <52994716+o4x@users.noreply.github.com>
2020-09-24 20:43:41 +0330

    Add artiast image to tageditor

--- app/src/main/java/com/o4x/musical/extensions/ActivityEx.kt
+++ app/src/main/java/com/o4x/musical/extensions/ActivityEx.kt
@@ -15,6 +15,9 @@
 package com.o4x.musical.extensions
 
 import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.provider.MediaStore
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -23,6 +26,7 @@ import androidx.fragment.app.FragmentTransaction
 import code.name.monkey.appthemehelper.util.ToolbarContentTintHelper
 import com.google.android.material.appbar.MaterialToolbar
 import com.o4x.musical.R
+import com.o4x.musical.ui.activities.tageditor.AbsTagEditorActivity
 
 fun AppCompatActivity.applyToolbar(toolbar: MaterialToolbar) {
     toolbar.setBackgroundColor(surfaceColor())
@@ -63,6 +67,20 @@ fun AppCompatActivity.replaceFragment(
             }
             commit()
         }
+}
+
+
+fun AppCompatActivity.startImagePicker(requestCode: Int) {
+    val intent = Intent(Intent.ACTION_PICK,
+        MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+    intent.type = "image/*"
+    intent.putExtra("crop", "true")
+    intent.putExtra("scale", true)
+    intent.putExtra("aspectX", 1)
+    intent.putExtra("aspectY", 1)
+    startActivityForResult(
+        intent,
+        requestCode)
 }
 
 inline fun <reified T : Any> Activity.extra(key: String, default: T? = null) = lazy {
