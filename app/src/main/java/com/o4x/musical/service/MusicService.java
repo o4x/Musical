005989f5fcb2deb071fed833b42cb3268d656d06
o4x <52994716+o4x@users.noreply.github.com>
2020-09-25 13:23:49 +0330

    Fix playlist updating

--- app/src/main/java/com/o4x/musical/service/MusicService.java
+++ app/src/main/java/com/o4x/musical/service/MusicService.java
@@ -224,8 +224,6 @@ public class MusicService extends Service implements SharedPreferences.OnSharedP
         mediaSession.setActive(true);
 
         sendBroadcast(new Intent("com.o4x.musical.PHONOGRAPH_MUSIC_SERVICE_CREATED"));
-
-        mediaStoreObserver.onChange(true);
     }
 
     private AudioManager getAudioManager() {
@@ -1341,7 +1339,6 @@ public class MusicService extends Service implements SharedPreferences.OnSharedP
             // actually call refresh when the delayed callback fires
             // do not send a sticky broadcast here
             handleAndSendChangeInternal(MEDIA_STORE_CHANGED);
-            onChange(true);
         }
     }
 
