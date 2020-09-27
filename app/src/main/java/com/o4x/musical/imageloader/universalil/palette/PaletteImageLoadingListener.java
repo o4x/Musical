094164e2934c965fe97cd52b0b8af577d6aba394
o4x <52994716+o4x@users.noreply.github.com>
2020-09-27 08:21:02 +0330

    make grid blend mode

--- app/src/main/java/com/o4x/musical/imageloader/universalil/palette/PaletteImageLoadingListener.java
+++ app/src/main/java/com/o4x/musical/imageloader/universalil/palette/PaletteImageLoadingListener.java
@@ -5,10 +5,12 @@ import android.graphics.Bitmap;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.ColorUtils;
 import androidx.palette.graphics.Palette;
 
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.o4x.musical.R;
+import com.o4x.musical.extensions.ColorExtKt;
 import com.o4x.musical.util.PhonographColorUtil;
 
 import code.name.monkey.appthemehelper.util.ATHUtil;
@@ -19,11 +21,17 @@ public abstract class PaletteImageLoadingListener extends AbsImageLoadingListene
     public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
         super.onLoadingFailed(imageUri, view, failReason);
         if (onFailedBitmap == null) {
-            onColorReady(getDefaultFooterColor(view.getContext()));
+            colorReady(
+                    view.getContext(),
+                    getDefaultFooterColor(view.getContext())
+            );
         } else {
-            onColorReady(
-                    PhonographColorUtil.getColor(Palette.from(onFailedBitmap).generate(),
-                            getDefaultFooterColor(view.getContext()))
+            colorReady(
+                    view.getContext(),
+                    PhonographColorUtil.getColor(
+                            Palette.from(onFailedBitmap).generate(),
+                            getDefaultFooterColor(view.getContext())
+                    )
             );
         }
     }
@@ -31,15 +39,18 @@ public abstract class PaletteImageLoadingListener extends AbsImageLoadingListene
     @Override
     public void onLoadingCancelled(String imageUri, View view) {
         super.onLoadingCancelled(imageUri, view);
-        onColorReady(getDefaultFooterColor(view.getContext()));
+        colorReady(view.getContext(), getDefaultFooterColor(view.getContext()));
     }
 
     @Override
     public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
         super.onLoadingComplete(imageUri, view, loadedImage);
-        onColorReady(
-                PhonographColorUtil.getColor(Palette.from(loadedImage).generate(),
-                getDefaultFooterColor(view.getContext()))
+        colorReady(
+                view.getContext(),
+                PhonographColorUtil.getColor(
+                        Palette.from(loadedImage).generate(),
+                        getDefaultFooterColor(view.getContext())
+                )
         );
     }
 
@@ -49,6 +60,16 @@ public abstract class PaletteImageLoadingListener extends AbsImageLoadingListene
 
     protected int getAlbumArtistFooterColor(@NonNull Context context) {
         return ATHUtil.INSTANCE.resolveColor(context, R.attr.cardBackgroundColor);
+    }
+
+    private void colorReady(Context context, int color) {
+        onColorReady(
+                ColorUtils.blendARGB(
+                        color,
+                        ColorExtKt.surfaceColor(context),
+                        .5f
+                )
+        );
     }
 
     public abstract void onColorReady(int color);
