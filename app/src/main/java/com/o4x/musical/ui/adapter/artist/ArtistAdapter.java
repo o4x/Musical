8a5b31649c1c9c696996b94b54c444ca43a9f0cf
o4x <52994716+o4x@users.noreply.github.com>
2020-10-09 09:36:10 +0330

    Fix artist navigation animation

--- app/src/main/java/com/o4x/musical/ui/adapter/artist/ArtistAdapter.java
+++ app/src/main/java/com/o4x/musical/ui/adapter/artist/ArtistAdapter.java
@@ -195,11 +195,7 @@ public class ArtistAdapter extends AbsMultiSelectAdapter<ArtistAdapter.ViewHolde
             if (isInQuickSelectMode()) {
                 toggleChecked(getAdapterPosition());
             } else {
-                Pair[] artistPairs = new Pair[]{
-                        Pair.create(image,
-                                activity.getResources().getString(R.string.transition_artist_image)
-                        )};
-                NavigationUtil.goToArtist(activity, dataSet.get(getAdapterPosition()).getId(), artistPairs);
+                NavigationUtil.goToArtist(activity, dataSet.get(getAdapterPosition()).getId());
             }
         }
 
