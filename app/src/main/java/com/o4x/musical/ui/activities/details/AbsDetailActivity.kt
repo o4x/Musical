0c3d060447f4147f775922c8405825ffdaa2fdab
o4x <52994716+o4x@users.noreply.github.com>
2020-10-10 18:03:02 +0330

    covert java to kot

---
+++ app/src/main/java/com/o4x/musical/ui/activities/details/AbsDetailActivity.kt
@@ -0,0 +1,204 @@
+package com.o4x.musical.ui.activities.details
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Spanned
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.loader.app.LoaderManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+import butterknife.BindView
+import butterknife.ButterKnife
+import code.name.monkey.appthemehelper.util.ColorUtil.withAlpha
+import code.name.monkey.appthemehelper.util.ToolbarContentTintHelper
+import com.afollestad.materialcab.MaterialCab
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.util.DialogUtils
+import com.o4x.musical.R
+import com.o4x.musical.imageloader.universalil.listener.PaletteMusicLoadingListener
+import com.o4x.musical.imageloader.universalil.loader.UniversalIL
+import com.o4x.musical.interfaces.CabHolder
+import com.o4x.musical.interfaces.PaletteColorHolder
+import com.o4x.musical.model.Song
+import com.o4x.musical.ui.activities.base.AbsMusicPanelActivity
+import com.o4x.musical.ui.adapter.song.DetailsSongAdapter
+import com.o4x.musical.util.PhonographColorUtil
+import com.o4x.musical.util.Util
+import com.o4x.musical.util.color.MediaNotificationProcessor
+import kotlin.math.max
+import kotlin.math.min
+
+abstract class AbsDetailActivity<T> : AbsMusicPanelActivity(), PaletteColorHolder, CabHolder,
+    LoaderManager.LoaderCallbacks<T> {
+
+    @BindView(R.id.song_recycler)
+    lateinit var songRecyclerView: RecyclerView
+    @BindView(R.id.image)
+    lateinit var image: ImageView
+    @BindView(R.id.toolbar)
+    lateinit var toolbar: Toolbar
+
+    var cab: MaterialCab? = null
+    var imageHeight = 0
+    var colors: MediaNotificationProcessor? = null
+
+    var wiki: Spanned? = null
+    var wikiDialog: MaterialDialog? = null
+
+    lateinit var songAdapter: DetailsSongAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ButterKnife.bind(this)
+        setDrawUnderStatusBar()
+        setUpToolBar()
+        setupViews()
+        reload()
+    }
+
+    override fun createContentView(): View? {
+        return wrapSlidingMusicPanel(R.layout.activity_detail)
+    }
+
+    override fun getPaletteColor(): Int {
+        return colors!!.actionBarColor
+    }
+
+    override fun openCab(menuRes: Int, callback: MaterialCab.Callback): MaterialCab {
+        if (cab != null && cab!!.isActive) cab!!.finish()
+        cab = MaterialCab(this, R.id.cab_stub)
+            .setMenu(menuRes)
+            .setCloseDrawableRes(R.drawable.ic_close_white_24dp)
+            .setBackgroundColor(PhonographColorUtil.shiftBackgroundColorForLightText(paletteColor))
+            .start(object : MaterialCab.Callback {
+                override fun onCabCreated(materialCab: MaterialCab, menu: Menu): Boolean {
+                    return callback.onCabCreated(materialCab, menu)
+                }
+
+                override fun onCabItemClicked(menuItem: MenuItem): Boolean {
+                    return callback.onCabItemClicked(menuItem)
+                }
+
+                override fun onCabFinished(materialCab: MaterialCab): Boolean {
+                    return callback.onCabFinished(materialCab)
+                }
+            })
+        return cab!!
+    }
+
+    override fun onBackPressed() {
+        if (cab != null && cab!!.isActive) cab!!.finish() else {
+            songRecyclerView.stopScroll()
+            super.onBackPressed()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAG_EDITOR_REQUEST) {
+            reload()
+            setResult(RESULT_OK)
+        }
+    }
+
+    private fun setUpToolBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = null
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    open fun setupViews() {
+        setupScrollView()
+        setupSongsRecycler()
+        setColors(DialogUtils.resolveColor(this, R.attr.defaultFooterColor), null)
+    }
+
+    private fun setupScrollView() {
+        val displayHeight = Util.getScreenHeight()
+        imageHeight = Util.getScreenWidth()
+        val gradientHeight =
+            (imageHeight + resources.getDimension(R.dimen.detail_header_height)).toInt()
+        songRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            private var scrollY = 0
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                scrollY += dy
+
+                // Change alpha of overlay
+                val alpha = max(0f, min(1f, 2.toFloat() * scrollY / gradientHeight))
+                setAppbarAlpha(alpha)
+
+                // Scroll poster
+                image.translationY =
+                    max(-scrollY / (displayHeight * 2 / imageHeight), -imageHeight)
+                        .toFloat()
+            }
+        })
+    }
+
+    private fun setupSongsRecycler() {
+        songAdapter = DetailsSongAdapter(this, getSongs(), R.layout.item_list, false, this)
+        songRecyclerView.layoutManager = LinearLayoutManager(this)
+        songRecyclerView.adapter = songAdapter
+        songAdapter.registerAdapterDataObserver(object : AdapterDataObserver() {
+            override fun onChanged() {
+                super.onChanged()
+                if (songAdapter.itemCount == 0) finish()
+            }
+        })
+    }
+
+    private fun setColors(color: Int, colors: MediaNotificationProcessor?) {
+        if (colors != null) {
+            this.colors = colors
+            ToolbarContentTintHelper.colorizeToolbar(toolbar, colors.primaryTextColor, this)
+            setNavigationBarColor(colors.actionBarColor)
+            setTaskDescriptionColor(colors.actionBarColor)
+        }
+        findViewById<View>(android.R.id.content).rootView.setBackgroundColor(color)
+        setAppbarAlpha(0f)
+    }
+
+    private fun setAppbarAlpha(alpha: Float) {
+        if (colors == null) return
+        toolbar.setBackgroundColor(
+            withAlpha(colors!!.actionBarColor, alpha)
+        )
+        setStatusBarColor(
+            withAlpha(colors!!.actionBarColor, alpha)
+        )
+        toolbar.setTitleTextColor(
+            withAlpha(colors!!.primaryTextColor, alpha)
+        )
+    }
+
+    override fun onMediaStoreChanged() {
+        super.onMediaStoreChanged()
+        reload()
+    }
+
+    val imageLoader: UniversalIL
+        get() = UniversalIL(
+            image,
+            object : PaletteMusicLoadingListener() {
+                override fun onColorReady(colors: MediaNotificationProcessor) {
+                    setColors(colors.backgroundColor, colors)
+                    setMiniPlayerColor(colors)
+                    songAdapter.colors = colors
+                }
+            }, imageHeight
+        )
+
+    abstract fun loadImage()
+    abstract fun reload()
+    protected abstract fun getSongs(): List<Song>
+
+    companion object {
+        const val TAG_EDITOR_REQUEST = 2001
+    }
+}
---
+++ app/src/main/java/com/o4x/musical/ui/activities/details/ArtistDetailActivity.kt
@@ -0,0 +1,220 @@
+package com.o4x.musical.ui.activities.details
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.loader.content.Loader
+import com.afollestad.materialdialogs.MaterialDialog
+import com.o4x.musical.R
+import com.o4x.musical.helper.MusicPlayerRemote.enqueue
+import com.o4x.musical.helper.MusicPlayerRemote.openAndShuffleQueue
+import com.o4x.musical.helper.MusicPlayerRemote.playNext
+import com.o4x.musical.interfaces.LoaderIds
+import com.o4x.musical.misc.WrappedAsyncTaskLoader
+import com.o4x.musical.model.Artist
+import com.o4x.musical.model.Artist.Companion.empty
+import com.o4x.musical.model.Song
+import com.o4x.musical.repository.RealAlbumRepository
+import com.o4x.musical.repository.RealArtistRepository
+import com.o4x.musical.repository.RealSongRepository
+import com.o4x.musical.ui.activities.details.ArtistDetailActivity
+import com.o4x.musical.ui.activities.tageditor.AbsTagEditorActivity
+import com.o4x.musical.ui.activities.tageditor.ArtistTagEditorActivity
+import com.o4x.musical.ui.dialogs.AddToPlaylistDialog
+import com.o4x.musical.ui.dialogs.SleepTimerDialog
+import com.o4x.musical.util.CustomImageUtil
+import com.o4x.musical.util.NavigationUtil
+import com.o4x.musical.util.PreferenceUtil.isAllowedToDownloadMetadata
+import java.util.*
+
+/**
+ * Be careful when changing things in this Activity!
+ */
+class ArtistDetailActivity : AbsDetailActivity<Artist?>() {
+
+    private var artist: Artist? = null
+    public override fun loadImage() {
+        imageLoader.loadImage(artist!!)
+    }
+
+    public override fun reload() {
+        supportLoaderManager.restartLoader(LOADER_ID, intent.extras, this)
+    }
+
+    private fun loadBiography(lang: String? = Locale.getDefault().language) {
+        wiki = null
+
+//        ApiClient.getClient(this).create(LastFMService.class)
+//                .getArtistInfo(getArtist().getName(), lang, null)
+//                .enqueue(new Callback<LastFmArtist>() {
+//                    @Override
+//                    public void onResponse(@NonNull Call<LastFmArtist> call, @NonNull Response<LastFmArtist> response) {
+//                        final LastFmArtist lastFmArtist = response.body();
+//                        if (lastFmArtist != null && lastFmArtist.getArtist() != null) {
+//                            final String bioContent = lastFmArtist.getArtist().getBio().getContent();
+//                            if (bioContent != null && !bioContent.trim().isEmpty()) {
+//                                biography = Html.fromHtml(bioContent);
+//                            }
+//                        }
+//
+//                        // If the "lang" parameter is set and no biography is given, retry with default language
+//                        if (biography == null && lang != null) {
+//                            loadBiography(null);
+//                            return;
+//                        }
+//
+//                        if (!PreferenceUtil.isAllowedToDownloadMetadata(ArtistDetailActivity.this)) {
+//                            if (biography != null) {
+//                                biographyDialog.setContent(biography);
+//                            } else {
+//                                biographyDialog.dismiss();
+//                                Toast.makeText(ArtistDetailActivity.this, getResources().getString(R.string.biography_unavailable), Toast.LENGTH_SHORT).show();
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(@NonNull Call<LastFmArtist> call, @NonNull Throwable t) {
+//                        t.printStackTrace();
+//                        biography = null;
+//                    }
+//                });
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_artist_detail, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        val songs = songAdapter.dataSet
+        when (id) {
+            R.id.action_sleep_timer -> {
+                SleepTimerDialog().show(supportFragmentManager, "SET_SLEEP_TIMER")
+                return true
+            }
+            R.id.action_equalizer -> {
+                NavigationUtil.openEqualizer(this)
+                return true
+            }
+            R.id.action_shuffle_artist -> {
+                openAndShuffleQueue(songs, true)
+                return true
+            }
+            R.id.action_play_next -> {
+                playNext(songs)
+                return true
+            }
+            R.id.action_add_to_current_playing -> {
+                enqueue(songs)
+                return true
+            }
+            R.id.action_add_to_playlist -> {
+                AddToPlaylistDialog.create(songs).show(supportFragmentManager, "ADD_PLAYLIST")
+                return true
+            }
+            android.R.id.home -> {
+                super.onBackPressed()
+                return true
+            }
+            R.id.action_biography -> {
+                if (wikiDialog == null) {
+                    wikiDialog = MaterialDialog.Builder(this)
+                        .title(artist!!.name)
+                        .positiveText(android.R.string.ok)
+                        .build()
+                }
+                if (isAllowedToDownloadMetadata(this@ArtistDetailActivity)) { // wiki should've been already downloaded
+                    if (wiki != null) {
+                        wikiDialog?.setContent(wiki)
+                        wikiDialog?.show()
+                    } else {
+                        Toast.makeText(
+                            this@ArtistDetailActivity,
+                            resources.getString(R.string.biography_unavailable),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else { // force download
+                    wikiDialog?.show()
+                    loadBiography()
+                }
+                return true
+            }
+            R.id.action_reset_artist_image -> {
+                Toast.makeText(
+                    this@ArtistDetailActivity,
+                    resources.getString(R.string.updating),
+                    Toast.LENGTH_SHORT
+                ).show()
+                CustomImageUtil(artist).resetCustomImage()
+                return true
+            }
+            R.id.action_tag_editor -> {
+                val editor = Intent(this, ArtistTagEditorActivity::class.java)
+                editor.putExtra(AbsTagEditorActivity.EXTRA_ID, getArtist().id)
+                startActivityForResult(editor, TAG_EDITOR_REQUEST)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setArtist(artist: Artist) {
+        this.artist = artist
+        loadImage()
+        if (isAllowedToDownloadMetadata(this)) {
+            loadBiography()
+        }
+        supportActionBar!!.title = artist.name
+        //        songCountTextView.setText(MusicUtil.getSongCountString(this, artist.getSongCount()));
+//        albumCountTextView.setText(MusicUtil.getAlbumCountString(this, artist.getAlbumCount()));
+//        durationTextView.setText(MusicUtil.getReadableDurationString(MusicUtil.getTotalDuration(this, artist.getSongs())));
+        songAdapter.swapDataSet(artist.songs)
+        songAdapter.data = artist
+    }
+
+    override fun getSongs(): List<Song> {
+        return getArtist().songs
+    }
+
+    private fun getArtist(): Artist {
+        if (artist == null) artist = empty
+        return artist!!
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Artist?> {
+        return AsyncArtistDataLoader(this, args!!.getLong(EXTRA_ARTIST_ID))
+    }
+
+    override fun onLoadFinished(loader: Loader<Artist?>, data: Artist?) {
+        if (data != null) {
+            setArtist(data)
+        }
+    }
+
+    override fun onLoaderReset(loader: Loader<Artist?>) {
+        artist = empty
+        songAdapter.swapDataSet(artist!!.songs)
+        //        albumAdapter.swapDataSet(artist.getAlbums());
+    }
+
+    private class AsyncArtistDataLoader(context: Context?, private val artistId: Long) :
+        WrappedAsyncTaskLoader<Artist?>(context) {
+        override fun loadInBackground(): Artist? {
+            return RealArtistRepository(
+                RealSongRepository(context),
+                RealAlbumRepository(RealSongRepository(context))
+            ).artist(artistId)
+        }
+    }
+
+    companion object {
+        private const val LOADER_ID = LoaderIds.ARTIST_DETAIL_ACTIVITY
+        const val EXTRA_ARTIST_ID = "extra_artist_id"
+    }
+}
