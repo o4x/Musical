df60db30f5e0adbeaf18b2792455d0e940aa84b4
o4x <52994716+o4x@users.noreply.github.com>
2020-11-06 21:12:26 +0330

    Fix styles

--- app/src/main/java/com/o4x/musical/ui/fragments/player/AbsPlayerFragment.java
+++ app/src/main/java/com/o4x/musical/ui/fragments/player/AbsPlayerFragment.java
@@ -208,7 +208,7 @@ public abstract class AbsPlayerFragment extends AbsMusicServiceFragment
 
     private void setUpPlayerToolbar() {
         toolbar.inflateMenu(R.menu.menu_player);
-        toolbar.setNavigationIcon(R.drawable.icon_back_white);
+        toolbar.setNavigationIcon(R.drawable.ic_keyboard_backspace);
         toolbar.setNavigationOnClickListener(v -> getActivity().onBackPressed());
         toolbar.setOnMenuItemClickListener(this);
     }
