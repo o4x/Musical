3ca1ba8601c16801afc03be0c4134e112d06a032
o4x <52994716+o4x@users.noreply.github.com>
2020-11-15 16:54:55 +0330

    Fix database bug

--- app/src/main/java/com/o4x/musical/db/RetroDatabase.kt
+++ app/src/main/java/com/o4x/musical/db/RetroDatabase.kt
@@ -4,8 +4,16 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [PlaylistEntity::class, SongEntity::class, HistoryEntity::class, PlayCountEntity::class, QueueEntity::class, LyricsEntity::class],
-    version = 3,
+    entities = [
+        PlaylistEntity::class,
+        SongEntity::class,
+        HistoryEntity::class,
+        PlayCountEntity::class,
+        QueueEntity::class,
+        QueueSongEntity::class,
+        LyricsEntity::class
+    ],
+    version = 4,
     exportSchema = false
 )
 abstract class RetroDatabase : RoomDatabase() {
