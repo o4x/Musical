c328c94fec3e3956d9eeda2972e52e0f80516381
o4x <52994716+o4x@users.noreply.github.com>
2020-11-27 10:04:19 +0330

    Fix drawer

--- app/src/main/java/com/o4x/musical/ui/activities/MainActivity.kt
+++ app/src/main/java/com/o4x/musical/ui/activities/MainActivity.kt
@@ -85,7 +85,6 @@ class MainActivity : AbsMusicPanelActivity(), CabHolder {
     }
 
     override fun createContentView(): View {
-
         return wrapSlidingMusicPanel(R.layout.activity_main)
     }
 
