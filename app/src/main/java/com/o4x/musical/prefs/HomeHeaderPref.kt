6e9d5541be27fbd93d21f0494219008364d53d0e
o4x <52994716+o4x@users.noreply.github.com>
2020-12-08 11:04:09 +0330

    Finish select image

--- app/src/main/java/com/o4x/musical/prefs/HomeHeaderPref.kt
+++ app/src/main/java/com/o4x/musical/prefs/HomeHeaderPref.kt
@@ -3,40 +3,48 @@ package com.o4x.musical.prefs
 import android.content.Context
 import android.content.SharedPreferences
 import com.o4x.musical.App
+import java.text.SimpleDateFormat
+import java.util.*
 
 object HomeHeaderPref {
 
     // types
     const val TYPE_CUSTOM = 0
-    const val TYPE_TAG = 1
+    const val TYPE_SONG = 1
     const val TYPE_DEFAULT = 2
 
+    const val CHANGE = "change"
     private const val TYPE = "type"
     private const val CUSTOM_IMAGE_PATH = "custom_image_path"
     private const val SONG_ID = "song_id"
-    private const val INDEX = "index"
 
     private val sp: SharedPreferences =
         App.getContext().getSharedPreferences("key_home_header_pref", Context.MODE_PRIVATE)
 
     @JvmStatic
     fun registerOnSharedPreferenceChangedListener(
-        sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener?) {
+        sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener?
+    ) {
         sp.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
     }
 
     @JvmStatic
     fun unregisterOnSharedPreferenceChangedListener(
-        sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener?) {
+        sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener?
+    ) {
         sp.unregisterOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
     }
 
     @JvmStatic
     var homeHeaderType: Int
         get() = sp.getInt(TYPE, TYPE_DEFAULT)
-        set(value) {
+        private set(value) {
             val editor = sp.edit()
             editor.putInt(TYPE, value)
+            editor.putLong(
+                CHANGE,
+                System.currentTimeMillis()
+            )
             editor.apply()
         }
 
@@ -47,6 +55,7 @@ object HomeHeaderPref {
             val editor = sp.edit()
             editor.putString(CUSTOM_IMAGE_PATH, value)
             editor.apply()
+            homeHeaderType = TYPE_CUSTOM
         }
 
     @JvmStatic
@@ -56,15 +65,11 @@ object HomeHeaderPref {
             val editor = sp.edit()
             editor.putLong(SONG_ID, value)
             editor.apply()
+            homeHeaderType = TYPE_SONG
         }
-
 
     @JvmStatic
-    var defaultImageIndex: Int
-        get() = sp.getInt(INDEX, 0)
-        set(value) {
-            val editor = sp.edit()
-            editor.putInt(INDEX, value)
-            editor.apply()
-        }
+    fun setDefault() {
+        homeHeaderType = TYPE_DEFAULT
+    }
 }
--- app/src/main/java/com/o4x/musical/ui/fragments/settings/homehader/HomeHeaderFragment.kt
+++ app/src/main/java/com/o4x/musical/ui/fragments/settings/homehader/HomeHeaderFragment.kt
@@ -3,6 +3,7 @@ package com.o4x.musical.ui.fragments.settings.homehader
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,13 +11,14 @@ import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
 import com.github.dhaval2404.imagepicker.ImagePicker
 import com.o4x.musical.App
 import com.o4x.musical.R
 import com.o4x.musical.databinding.FragmentHomeHeaderBinding
+import com.o4x.musical.imageloader.glide.module.GlideApp
 import com.o4x.musical.prefs.HomeHeaderPref
 import com.o4x.musical.ui.activities.MusicPickerActivity
-import com.o4x.musical.ui.fragments.mainactivity.datails.AbsDetailFragment
 import com.o4x.musical.ui.viewmodel.HomeHeaderViewModel
 import com.o4x.musical.util.MusicUtil
 import kotlinx.coroutines.Dispatchers
@@ -55,53 +57,28 @@ class HomeHeaderFragment : Fragment(R.layout.fragment_home_header) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getPosterBitmap().observe(viewLifecycleOwner, {
-            binding.image.setImageBitmap(it)
+            GlideApp.with(this)
+                .asBitmap()
+                .load(it)
+                .transition(BitmapTransitionOptions.withCrossFade())
+                .into(binding.image)
         })
 
-        when (HomeHeaderPref.homeHeaderType) {
-            HomeHeaderPref.TYPE_CUSTOM -> {
-                binding.radioCustom.isChecked = true
+        binding.apply {
+            useCustom.setOnClickListener {
+                startHomeHeaderImagePicker()
             }
-            HomeHeaderPref.TYPE_TAG -> {
-                binding.radioTag.isChecked = true
-            }
-            HomeHeaderPref.TYPE_DEFAULT -> {
-                binding.radioDefault.isChecked = true
-            }
-        }
-
-        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                binding.radioCustom.id -> {
-                    HomeHeaderPref.homeHeaderType = HomeHeaderPref.TYPE_CUSTOM
-                    startHomeHeaderImagePicker()
-                }
-                binding.radioTag.id -> {
-                    HomeHeaderPref.homeHeaderType = HomeHeaderPref.TYPE_TAG
-
-                    val myIntent = Intent(
-                        requireContext(),
-                        MusicPickerActivity::class.java
-                    )
-                    startActivityForResult(myIntent, REQUEST_CODE_SELECT_SONG)
-                }
-                binding.radioDefault.id -> {
-                    HomeHeaderPref.homeHeaderType = HomeHeaderPref.TYPE_DEFAULT
-                }
-            }
-        }
-
-        binding.recyclerView.adapter = DefaultImageRecyclerView()
-        binding.recyclerView.layoutManager = object: LinearLayoutManager(
-            requireContext(), LinearLayoutManager.HORIZONTAL, false
-        ) {
-
-            override fun checkLayoutParams(lp: RecyclerView.LayoutParams): Boolean {
-                lp.width = (width / 3) - (lp.marginStart * 2 /* for left and right */)
-                lp.height = (lp.width * 1.5).toInt()
-                return super.checkLayoutParams(lp)
+            useSong.setOnClickListener {
+                val myIntent = Intent(
+                    requireContext(),
+                    MusicPickerActivity::class.java
+                )
+                startActivityForResult(myIntent, REQUEST_CODE_SELECT_SONG)
             }
 
+            useDefault.setOnClickListener {
+                HomeHeaderPref.setDefault()
+            }
         }
     }
 
