d58cb2f7270b255484cc85cc74a3b84ed842e7c0
o4x <52994716+o4x@users.noreply.github.com>
2020-12-12 00:05:47 +0330

    Change blur method

--- app/src/main/java/com/o4x/musical/ui/adapter/cover/AlbumCoverPagerAdapter.kt
+++ app/src/main/java/com/o4x/musical/ui/adapter/cover/AlbumCoverPagerAdapter.kt
@@ -1,10 +1,19 @@
 package com.o4x.musical.ui.adapter.cover
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
+import com.o4x.musical.App
 import com.o4x.musical.imageloader.glide.loader.GlideLoader
+import com.o4x.musical.imageloader.glide.module.GlideApp
+import com.o4x.musical.imageloader.glide.targets.CustomBitmapTarget
+import com.o4x.musical.imageloader.glide.targets.palette.AbsPaletteTargetListener
+import com.o4x.musical.imageloader.glide.transformation.blur.BlurTransformation
 import com.o4x.musical.model.Song
+import com.o4x.musical.util.Util
 import com.o4x.musical.util.color.MediaNotificationProcessor
 
 /**
@@ -21,10 +30,23 @@ class AlbumCoverPagerAdapter(fm: FragmentManager?, dataSet: List<Song>) :
 
         override fun loadAlbumCover() {
             GlideLoader.with(requireContext())
-
+                .withListener(object : AbsPaletteTargetListener(requireContext()) {
+                    override fun onResourceReady(resource: Bitmap?) {
+                        super.onResourceReady(resource)
+                        if (resource == null) return
+                        _binding?.playerImage?.let {
+                            GlideApp.with(App.getContext())
+                                .asBitmap()
+                                .transition(BitmapTransitionOptions.withCrossFade())
+                                .transform(BlurTransformation(100, 1))
+                                .load(resource)
+                                .into(it)
+                        }
+                    }
+                }.also { it.loadPlaceholderPalette = true })
                 .withBlur(100f)
                 .load(song)
-                .into(binding.playerImage)
+                .into(CustomBitmapTarget(Util.getScreenWidth(), Util.getScreenHeight()))
         }
 
         companion object {
