9492791f93b32a345b6ce21ed92bd7c78bbc7b1b
o4x <52994716+o4x@users.noreply.github.com>
2020-12-14 09:45:40 +0330

    Update AddToPlaylistDialog.kt

--- app/src/main/java/com/o4x/musical/ui/dialogs/AddToPlaylistDialog.kt
+++ app/src/main/java/com/o4x/musical/ui/dialogs/AddToPlaylistDialog.kt
@@ -2,20 +2,14 @@ package com.o4x.musical.ui.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.DialogFragment
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.list.listItems
-import com.afollestad.materialdialogs.list.updateListItems
 import com.o4x.musical.R
 import com.o4x.musical.model.Playlist
 import com.o4x.musical.model.Song
 import com.o4x.musical.ui.activities.base.AbsMusicServiceActivity
-import com.o4x.musical.ui.fragments.AbsMusicServiceFragment
-import com.o4x.musical.ui.viewmodel.LibraryViewModel
 import com.o4x.musical.util.PlaylistsUtil
-import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import java.util.*
 
 /**
