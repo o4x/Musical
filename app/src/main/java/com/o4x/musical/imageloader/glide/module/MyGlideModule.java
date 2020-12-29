3af6f3adacbf3c041c018e67b3cedf4c6401d9b9
o4x <52994716+o4x@users.noreply.github.com>
2020-12-29 23:21:49 +0330

    Fix image loading blocking

--- app/src/main/java/com/o4x/musical/imageloader/glide/module/MyGlideModule.java
+++ app/src/main/java/com/o4x/musical/imageloader/glide/module/MyGlideModule.java
@@ -32,13 +32,13 @@ public class MyGlideModule extends AppGlideModule {
 
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
-        builder.setLogLevel(Log.ERROR);
+//        builder.setLogLevel(Log.ERROR);
         super.applyOptions(context, builder);
     }
 
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
-        registry.append(ArtistImage.class, InputStream.class, new ArtistImageFactory());
+        registry.append(ArtistImage.class, InputStream.class, new ArtistImageFactory(context));
         registry.append(AudioFileCover.class, InputStream.class, new AudioFileCoverLoader.Factory());
         registry.append(MultiImage.class, InputStream.class, new MosaicImageLoader.Factory());
         super.registerComponents(context, glide, registry);
