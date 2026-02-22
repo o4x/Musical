118b5a1254c328aaf45cd6e27fe18f6a27cc5635
Reza <8reza0@gmail.com>
2026-02-22 09:37:38 +0330

    Improve theme and queue fragment

--- app/src/main/java/github/o4x/musical/ui/fragments/mainactivity/queue/QueueFragment.kt
+++ app/src/main/java/github/o4x/musical/ui/fragments/mainactivity/queue/QueueFragment.kt
@@ -1,7 +1,11 @@
 package github.o4x.musical.ui.fragments.mainactivity.queue
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.RecyclerView
 import com.h6ah4i.android.widget.advrecyclerview.animator.GeneralItemAnimator
 import com.h6ah4i.android.widget.advrecyclerview.animator.RefactoredDefaultItemAnimator
@@ -10,13 +14,18 @@ import github.o4x.musical.R
 import github.o4x.musical.databinding.FragmentQueueBinding
 import github.o4x.musical.helper.MusicPlayerRemote
 import github.o4x.musical.misc.OverScrollLinearLayoutManager
+import github.o4x.musical.prefs.HomeHeaderPref
+import github.o4x.musical.ui.activities.MusicPickerActivity
 import github.o4x.musical.ui.adapter.song.PlayingQueueAdapter
 import github.o4x.musical.ui.dialogs.CreatePlaylistDialog
 import github.o4x.musical.ui.fragments.mainactivity.AbsQueueFragment
+import github.o4x.musical.ui.fragments.mainactivity.home.HomeFragment.Companion.REQUEST_CODE_SELECT_SONG
+import github.o4x.musical.util.ViewInsetsUtils.applyAppBarPadding
+import github.o4x.musical.util.ViewInsetsUtils.applySystemBarsPadding
 import github.o4x.musical.util.ViewUtil
 import github.o4x.musical.util.accentColor
 
-class QueueFragment : AbsQueueFragment(R.layout.fragment_queue) {
+class QueueFragment : AbsQueueFragment(R.layout.fragment_queue), MenuProvider {
 
     private lateinit var wrappedAdapter: RecyclerView.Adapter<*>
     private lateinit var recyclerViewDragDropManager: RecyclerViewDragDropManager
@@ -43,30 +52,32 @@ class QueueFragment : AbsQueueFragment(R.layout.fragment_queue) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mainActivity.setSupportActionBar(binding.toolbar)
+        mainActivity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+        binding.appbar.applySystemBarsPadding(applyTop = true)
+        binding.queueRecyclerView.applyAppBarPadding()
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         setUpViews()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_queue, menu)
-        super.onCreateOptionsMenu(menu, inflater)
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_queue, menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_shuffle_all -> {
-                libraryViewModel.shuffleSongs()
-                return true
-            }
-            R.id.action_new_playlist -> {
-                CreatePlaylistDialog.create().show(childFragmentManager, "CREATE_PLAYLIST")
-                return true
-            }
-            R.id.action_search -> {
-                mainActivity.openSearch()
-                return true
-            }
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.action_shuffle_all -> libraryViewModel.shuffleSongs()
+            R.id.action_new_playlist -> CreatePlaylistDialog.create().show(childFragmentManager, "CREATE_PLAYLIST")
+            R.id.action_search -> mainActivity.openSearch()
         }
-        return super.onOptionsItemSelected(item)
+        return true
     }
 
     private fun setUpViews() {
