02a90cc81784520a09bebb80b7cd1c4e2d3b77c0
Reza <8reza0@gmail.com>
2026-06-30 21:06:00 +0330

    Prepare for compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,11 +95,15 @@ dependencies {
     implementation(files("libs/jaudiotagger-2.2.4-SNAPSHOT.jar"))
 
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.koin.androidx.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
